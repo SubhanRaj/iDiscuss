a4e0d445ae90402a119a5e5bac46a0d0c5f5a6ae
SubhanRaj <shubhanraj2002@gmail.com>
2022-01-24 12:36:59 +0530

    footer

---
+++ partials/_footer.php
@@ -0,0 +1,10 @@
+<div class="container-fluid bg-dark text-light">
+    <p class="text-center  mb-0">
+        &COPY; 2077 || iDiscuss : A PHP Forum
+    </p>
+</div>
+
+
+
+<!-- Always make sure to put Boot strap JS at last -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
