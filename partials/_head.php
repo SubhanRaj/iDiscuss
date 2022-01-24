686cdd41da2b1c5fdf81bf1aebe6903f2ab06677
SubhanRaj <shubhanraj2002@gmail.com>
2022-01-24 12:36:34 +0530

    <head> tag content

---
+++ partials/_head.php
@@ -0,0 +1,9 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Chrome android browser title colour -->
+<meta name="theme-color" content="#000000">
+<link rel="shortcut icon" href="iDiscuss.png" type="image/x-icon">
+
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
