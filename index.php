77c616fa3823d1f5eef01d38f6b44183f221d9b2
SubhanRaj <shubhanraj2002@gmail.com>
2022-01-24 12:37:10 +0530

    Basic layout of forum

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php require 'partials/_head.php'; ?>
+    <title>iDiscuss</title>
+</head>
+
+<body>
+    <?php require 'partials/_header.php'; ?>
+
+<div class="container">
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum error molestias tempora sint, quae inventore necessitatibus corrupti facere repellendus ipsum possimus voluptatibus aperiam tenetur consectetur! Harum eius libero dignissimos porro iure voluptas veniam rerum architecto obcaecati in consequuntur ratione sapiente cum, molestiae dolore facere tempore temporibus quos, rem alias maiores nostrum. Pariatur quia nulla aspernatur optio aliquid unde eaque! Neque, quod impedit. Sint expedita nobis obcaecati maxime nisi, vitae blanditiis officia suscipit. Repudiandae iure minima earum sunt alias impedit illo facilis dicta adipisci? Iusto natus cumque sed, laudantium voluptatem quisquam consequatur esse reiciendis, nihil similique, modi aliquam quibusdam at possimus.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio et mollitia nesciunt natus eos repellat molestias commodi obcaecati quo eius vitae, voluptatum pariatur totam odio facere adipisci dolor, reprehenderit quas provident porro amet? Provident fuga voluptate quisquam laboriosam. Laudantium nobis repellendus dolor aliquid nam, doloremque voluptatibus eos, perferendis veritatis animi ea, sapiente ratione porro repellat? Aperiam, quaerat dolore velit accusantium quam at nostrum vero tempore magnam fugit nulla ullam ratione nesciunt pariatur odit beatae cum. Incidunt temporibus fugiat atque earum. Fugit porro labore voluptate temporibus perferendis ullam autem doloremque laudantium, inventore assumenda recusandae alias dolor ipsum. Explicabo, neque inventore expedita illum hic odit dolore velit ipsam ea illo harum repellat tenetur mollitia quam magni laboriosam tempore laudantium nihil cumque voluptas temporibus excepturi? Impedit ut non possimus facere reprehenderit commodi unde expedita repellendus voluptas corporis tempora, cupiditate dignissimos quaerat amet quisquam voluptates sed dolores, cum atque facilis necessitatibus aliquid, delectus voluptatum a. Pariatur, a sed illo deleniti exercitationem eligendi maiores, nostrum doloribus repudiandae at, ducimus eius officia expedita cupiditate. Facilis, ex dolorum nesciunt adipisci, quos alias accusantium officiis et cupiditate at sequi vitae. Magni repudiandae et, est deleniti autem aut dolor beatae minus obcaecati fuga iure consectetur, cumque quos. Aliquam, suscipit!
+    </p>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur vero numquam obcaecati voluptatum quis consequuntur ipsa accusantium accusamus minus nisi, sit beatae placeat. Adipisci iste perferendis molestias libero provident, illo quis, eveniet sed natus debitis facere nisi repellendus aspernatur praesentium obcaecati reprehenderit? Inventore quam nihil nisi harum aperiam, sint cupiditate nesciunt eos nam ratione eius cumque molestias! Dolor soluta quae officiis asperiores fugit vero aspernatur, ut iste, eum numquam totam praesentium error dignissimos neque amet reprehenderit mollitia possimus, atque ipsa voluptate necessitatibus eos? Atque modi iusto, quo nemo quae at eius, aliquid aspernatur expedita impedit labore tempora totam iure accusantium molestias eum consequuntur? Quibusdam explicabo voluptate amet nostrum maiores tempora rerum sed dolorem sequi. Expedita quisquam fuga, dignissimos maiores illo molestiae vitae, vero dolor placeat, nobis ipsum ut itaque fugit quam alias dolorem sequi? Nam, cumque eius quisquam sequi est, architecto omnis odit sit quaerat, pariatur et deleniti! Ex repellendus quibusdam nemo explicabo. Magnam qui quaerat, ipsam amet esse enim quis porro aliquam architecto et modi doloribus, omnis aut repudiandae quisquam consectetur similique explicabo dolorum excepturi blanditiis alias veritatis tempore? Ab vitae suscipit voluptatum, aliquid facere voluptatem quibusdam et hic, accusamus ad dicta quisquam necessitatibus impedit sunt sint facilis! Placeat fuga eveniet voluptates quae asperiores soluta dicta laboriosam sunt cupiditate quas maiores qui, perspiciatis iste. Ipsam commodi sequi, asperiores fugit, facilis est praesentium molestias aliquid autem esse qui nulla quia. Veritatis voluptate nulla in earum, perspiciatis velit ex quae itaque, alias, voluptates delectus accusamus odio ipsam rem debitis sequi dicta sint consectetur fuga suscipit odit officia quia! Aut recusandae, fugiat minus saepe iste excepturi enim cumque quam ducimus nisi tenetur, ullam illo aperiam iusto soluta magni consequuntur illum molestias in quaerat animi quibusdam asperiores! Repellendus eaque, nam magni, iure sequi minima sit nostrum et rerum porro voluptatem quaerat maiores velit.
+    </p>
+</div>
+
+
+
+
+
+
+
+
+    <?php require 'partials/_footer.php'; ?>
+</body>
+
+</html>
