d675238ee3431745072e5f7a5bdacc3447574ddb
SubhanRaj <shubhanraj2002@gmail.com>
2022-01-24 20:55:11 +0530

    Changed from Bootstrap 5.6 to 4.5

---
+++ threads.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php require 'partials/_head.php'; ?>
+    <title>iDiscuss : Template</title>
+</head>
+
+<body>
+    <!-- Header -->
+    <?php require 'partials/_header.php'; ?>
+
+    <div class="container my-4">
+        <div class="jumbotron">
+            <h1 class="display-4">Hello, world!</h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention
+                to featured content or information.</p>
+            <hr class="my-4">
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </div>
+
+
+    </div>
+
+
+    <!-- Footer -->
+    <?php require 'partials/_footer.php'; ?>
+</body>
+
+</html>
