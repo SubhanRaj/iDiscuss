793149e8717c17ab98386d1c928f3e3a9a46497c
SubhanRaj <shubhanraj2002@gmail.com>
2022-01-24 14:18:26 +0530

    About page

---
+++ about.php
@@ -0,0 +1,20 @@
+<!-- Page Template -->
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php require 'partials/_head.php'; ?>
+    <title>About iDiscuss</title>
+</head>
+
+<body>
+    <!-- Header -->
+    <?php require 'partials/_header.php'; ?>
+    <!-- Content Here -->
+
+    <!-- Footer -->
+    <?php require 'partials/_footer.php'; ?>
+</body>
+
+</html>
