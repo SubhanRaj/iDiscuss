683de3d8525d7110f3b31b0e0ec2889ad536a2c8
SubhanRaj <shubhanraj2002@gmail.com>
2022-02-03 20:33:34 +0530

    Now Working

--- partials/_handleLogin.php
+++ partials/_handleLogin.php
@@ -8,36 +8,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST['loginPassword'];
     $sql = "SELECT * FROM `users` WHERE `userEmail` = '$email';";
     $result = mysqli_query($conn, $sql);
-    echo $result;
+    // echo $result;
     // Show SQL in case of error
 
-    // if (!$result) {
-    //     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    // }
+    if (!$result) {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
 
 
     $numRows = mysqli_num_rows($result);
     if ($numRows == 1) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            if (password_verify($password, $row['userPass'])) {
-
-                session_start();
-                $_SESSION['loggedin'] = true;
-                $_SESSION['username'] = $email;
-                echo 'Logged in with email: ' . $email;
-                header("Location : /iDiscuss/index.php");
-                exit();
-            } 
-            else {
-                $showError = "Password is incorrect.";
-                echo $showError;
-                // header("Location : /iDiscuss/index.php?error=$showError Password = $password");
-                // echo 'Password is incorrect.' . $password;
-            }
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($pass, $row['userPass'])) {
+            session_start();
+            $_SESSION['loggedin'] = true;
+            $_SESSION['useremail'] = $email;
+            echo "logged in" . $email;
+            header("Location: /iDiscuss/index.php");
         }
+        
     }
-} else {
-    $showError = "Invalid username or password";
-    echo $showError;
+    header("Location: /iDiscuss/index.php");  
 }
+// else {
+//     $showError = "Invalid username or password";
+//     echo $showError;
+// }
 ?>
