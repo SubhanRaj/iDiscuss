e9bf7acc0abf8f46456ecceb8d66cde6ea990879
SubhanRaj <shubhanraj2002@gmail.com>
2022-02-08 21:37:01 +0530

    Removed empty spaces

--- index.php
+++ index.php
@@ -26,28 +26,21 @@
                 echo '
                 <div class=" d-flex justify-content-center col-md-4 my-2">
                 <div class="card" style="width: 18rem;">
-                    <a href="/iDiscuss/threadlist.php?catid=' . $id . '"><img src="/iDiscuss/images/card/card-'.$id. '.png" height = "286" width = "286" class="card-img-top" alt="..."></a>
+                    <a href="/iDiscuss/threadlist.php?catid=' . $id . '"><img src="/iDiscuss/images/card/card-' . $id . '.png" height = "286" width = "286" class="card-img-top" alt="..."></a>
                     <div class="card-body">
                         <h5 class="card-title">' . $cat . '</h5>
-                        <p class="card-text">' .substr($desc, 0 , 180). '...</p>
-                        <a href="/iDiscuss/threadlist.php?catid='.$id.'" class="btn btn-primary">View Threads</a>
+                        <p class="card-text">' . substr($desc, 0, 180) . '...</p>
+                        <a href="/iDiscuss/threadlist.php?catid=' . $id . '" class="btn btn-primary">View Threads</a>
                     </div>
                 </div>
             </div> ';
             }
-
-
-
-
             ?>
-
         </div>
     </div>
 
     <div class="container">
-
     </div>
-
     <!-- Footer -->
     <?php require 'partials/_footer.php'; ?>
 </body>
